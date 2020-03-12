6240bf4da6180e3a50c173ea4b0a95bf7c2a0061
passa1248 <passa1248@gmail.com>
2020-03-12 04:00:59 +0100

    refactoring

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
-const https = require('https');
 const request = require("request-promise");
 
 var expressWs = require('express-ws')
 expressWs = expressWs(express());
 var app = expressWs.app;
 
-app.ws('/lastupdate', function (ws, req) {
-});
+app.ws('/lastupdate', function (ws, req) {});
 lastupdateWs = expressWs.getWss('/lastupdate');
 
 exports.lastUpdateWS = function (exchange) {
@@ -26,12 +24,9 @@ exports.lastUpdateWS = function (exchange) {
 const Bittrex = require('./exchanges/bittrex').Bittrex;
 const Binance = require('./exchanges/binance').Binance;
 const Poloniex = require('./exchanges/poloniex').Poloniex;
-//const Cryptopia = require('./exchanges/cryptopia').Cryptopia;
-//const Liqui = require('./exchanges/liqui').Liqui;
 const HitBTC = require('./exchanges/hitbtc').HitBTC;
 const Bitfinex = require('./exchanges/bitfinex').Bitfinex;
 const Exmo = require('./exchanges/exmo').Exmo;
-//const Huobipro = require('./exchanges/huobipro').Huobipro;
 const Qryptos = require('./exchanges/qryptos').Qryptos;
 
 var tickers = require("./common/variables.js").tickers;
@@ -39,16 +34,10 @@ var tickers = require("./common/variables.js").tickers;
 var queueBittrex = require("./common/variables.js").queueBittrex;
 var queuePoloniex = require("./common/variables.js").queuePoloniex;
 var queueBinance = require("./common/variables.js").queueBinance;
-//var queueCryptopia = require("./common/variables.js").queueCryptopia;
-//var queueLiqui = require("./common/variables.js").queueLiqui;
 var queueHitBTC = require("./common/variables.js").queueHitBTC;
 var queueBitfinex = require("./common/variables.js").queueBitfinex;
 var queueExmo = require("./common/variables.js").queueExmo;
-//var queueHuobipro = require("./common/variables.js").queueHuobipro;
 var queueQryptos = require("./common/variables.js").queueQryptos;
-
-
-// https://api.kraken.com/0/public/Ticker?pair=BCHEUR,BCHUSD,BCHXBT,DASHEUR,DASHUSD,DASHXBT,EOSETH,EOSEUR,EOSUSD,EOSXBT,GNOETH,GNOEUR,GNOUSD,GNOXBT,USDTUSD,ETCETH,ETCXBT,ETCEUR,ETCUSD,ETHXBT,ETHXBT.d,ETHCAD,ETHCAD.d,ETHEUR,ETHEUR.d,ETHGBP,ETHGBP.d,ETHJPY,ETHJPY.d,ETHUSD,ETHUSD.d,ICNETH,ICNXBT,LTCXBT,LTCEUR,LTCUSD,MLNETH,MLNXBT,REPETH,REPXBT,REPEUR,REPUSD,XBTCAD,XBTCAD.d,XBTEUR,XBTEUR.d,XBTGBP,XBTGBP.d,XBTJPY,XBTJPY.d,XBTUSD,XBTUSD.d,XDGXBT,XLMXBT,XLMEUR,XLMUSD,XMRXBT,XMREUR,XMRUSD,XRPXBT,XRPCAD,XRPEUR,XRPJPY,XRPUSD,ZECXBT,ZECEUR,ZECJPY,ZECUSD
 
 
 // Application insights
@@ -56,17 +45,6 @@ const appInsights = require("applicationinsights");
 appInsights.setup("26c089d4-a984-4155-9dd3-6c3890d64b9b")
   .setAutoCollectDependencies(false)
   .start();
-
-
-
-
-
-/*   setInterval(function () {
-    aWss.clients.forEach(function (client) {
-      client.send('hello');
-    });
-  }, 5000); */
-
 
 async function getOrderBook(exchange, type, market, res) {
 
@@ -79,32 +57,17 @@ async function getOrderBook(exchange, type, market, res) {
   };
   if (exchange == "Bittrex") {
     queueBittrex.push(params);
-  }
-  else if (exchange == "Poloniex") {
+  } else if (exchange == "Poloniex") {
     queuePoloniex.push(params);
-  }
-  else if (exchange == "Binance") {
+  } else if (exchange == "Binance") {
     queueBinance.push(params);
-  }
- /*  else if (exchange == "Cryptopia") {
-    queueCryptopia.push(params);
-  } */
- /*  else if (exchange == "Liqui") {
-    queueLiqui.push(params);
-  } */
-  else if (exchange == "Hitbtc") {
+  } else if (exchange == "Hitbtc") {
     queueHitBTC.push(params);
-  }
-  else if (exchange == "Bitfinex") {
+  } else if (exchange == "Bitfinex") {
     queueBitfinex.push(params);
-  }
-  else if (exchange == "Exmo") {
+  } else if (exchange == "Exmo") {
     queueExmo.push(params);
-  }
-  /*   else if (exchange == "Huobipro") {
-      queueHuobipro.push(params);
-    } */
-  else if (exchange == "Qryptos") {
+  } else if (exchange == "Qryptos") {
     queueQryptos.push(params);
   }
 }
@@ -113,12 +76,9 @@ async function getOrderBook(exchange, type, market, res) {
 Bittrex.startDequequeOrderbook();
 Poloniex.startDequequeOrderbook();
 Binance.startDequequeOrderbook();
-//Cryptopia.startDequequeOrderbook();
-//Liqui.startDequequeOrderbook();
 HitBTC.startDequequeOrderbook();
 Bitfinex.startDequequeOrderbook();
 Exmo.startDequequeOrderbook();
-//Huobipro.startDequequeOrderbook();
 Qryptos.startDequequeOrderbook();
 
 // Handlebars Middleware
@@ -128,7 +88,9 @@ app.engine('handlebars', exphbs({
 app.set('view engine', 'handlebars');
 
 // Body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(bodyParser.json());
 
 // Public file
@@ -197,71 +159,60 @@ app.get('/tickers', (req, res) => {
   res.send(JSON.stringify(json));
 });
 
-
-
 async function init() {
 
   try {
     await Bittrex.getTickers(true);
+  } catch (e) {
+    console.log(e);
   }
-  catch (e) { }
   try {
     await Binance.getTickers(true);
+  } catch (e) {
+    console.log(e);
   }
-  catch (e) { }
   try {
     await Poloniex.getTickers(true);
+  } catch (e) {
+    console.log(e);
   }
-  catch (e) { }
- /*  try {
-    await Cryptopia.getTickers(true);
-  }
-  catch (e) { } */
   try {
     await HitBTC.getTickers(true);
+  } catch (e) {
+    console.log(e);
   }
-  catch (e) { }
   try {
     await Bitfinex.getTickers(true);
+  } catch (e) {
+    console.log(e);
   }
-  catch (e) { }
   try {
     await Exmo.getTickers(true);
+  } catch (e) {
+    console.log(e);
   }
-  catch (e) { }
-/*   try {
-    await Liqui.getTickers(true);
-  }
-  catch (e) { } */
   try {
     await Qryptos.getTickers(true);
+  } catch (e) {
+    console.log(e);
   }
-  catch (e) { }
-  /*  try {
-    await Huobipro.fillPairs();
-  }
-  catch (e) { } */
+
   try {
     setTimeout(function () {
       RemoveAloneMarkets().then(function () {
         updateStatus();
         updateTickers();
         updateTickersSlow();
-        // Huobipro.refreshTickers();
-
-
       })
     }, 5000);
 
-  }
-  catch (e) { }
+  } catch (e) {}
 }
 
 function updateStatus() {
   setInterval(function () {
     Poloniex.getCurrencies();
     Bittrex.getCurrencies();
-  //  Cryptopia.getCurrencies();
     HitBTC.getCurrencies();
   }, 30000)
 }
@@ -273,7 +224,6 @@ function updateTickers() {
     Bittrex.getTickers(false);
     Binance.getTickers(false);
     Poloniex.getTickers(false);
-   // Liqui.getTickers(false);
     HitBTC.getTickers(false);
     Exmo.getTickers(false);
     Qryptos.getTickers(false);
@@ -283,7 +233,6 @@ function updateTickers() {
 function updateTickersSlow() {
   setInterval(function () {
     Bitfinex.getTickers(false);
-  //  Cryptopia.getTickers(false);
   }, 6000);
 }
 
@@ -299,20 +248,14 @@ async function RemoveAloneMarkets() {
       arr.push(1);
     if (tickers[i].bittrex.ask != undefined)
       arr.push(1);
- /*    if (tickers[i].cryptopia.ask != undefined)
-      arr.push(1); */
     if (tickers[i].binance.ask != undefined)
       arr.push(1);
-/*     if (tickers[i].liqui.ask != undefined)
-      arr.push(1); */
     if (tickers[i].hitbtc.ask != undefined)
       arr.push(1);
     if (tickers[i].bitfinex.ask != undefined)
       arr.push(1);
     if (tickers[i].exmo.ask != undefined)
       arr.push(1);
-    /*   if (tickers[i].huobipro.status != undefined)
-      arr.push(1); */
     if (tickers[i].qryptos.status != undefined)
       arr.push(1);
 
@@ -327,4 +270,3 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
-
